1b8babe9d62d49203dcc813a6ba05efa018f85e2
movinesta <movinesta@gmail.com>
2026-02-09 00:33:38 +0300

    ci: allow require in jest config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["jest.config.js", "jest.setup.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
